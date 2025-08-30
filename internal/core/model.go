94886ed20a01380fa0f14f24e40aca33d7ebba50
-Tod <fattttoddd@gmail.com>
2025-08-31 02:23:24 +0700

    Create model.go

---
+++ internal/core/model.go
@@ -0,0 +1,15 @@
+package core
+
+type User struct {
+    ID   string
+    Name string
+}
+
+// type Profile struct {
+//     ID     string
+//     UserID string
+//     Proto  string
+//     Server string
+//     Port   int
+//     Auth   map[string]string
+// }
